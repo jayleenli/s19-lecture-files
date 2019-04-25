8c5394da8511faff718df5461760a00b9b920368
shiyujiucsb <16054786+shiyujiucsb@users.noreply.github.com>
2019-04-24 21:52:54 -0700

    Create make_A_3D.py

---
+++ 04.23/make_A_3D.py
@@ -0,0 +1,13 @@
+from scipy import sparse
+
+def make_A_3D(n):
+    """
+    Create the matrix for the temperature problem on a k-by-k-by-k 3D grid.
+    Parameters:
+        k: number of grid points in each dimension.
+    Outputs:
+        A: the sparse k**3-by-k**3 matrix representing the finite difference approximation to Poisson's equation.
+    """
+    print("stub - remove me before submitting")
+
+    return
